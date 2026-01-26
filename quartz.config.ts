f6c9757ae70ca94a62ef4f9e0ac261b822e418f6
galapagh0st <alexmatthewcosta@gmail.com>
2026-01-26 14:25:36 -0500

    redo

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "&#128218;Alex Costa",
+    pageTitle: "✍🏻Alex Costa📚",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
